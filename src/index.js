601b0d188391c6779fd5de55ff4bbc99d7650a86
Sven Heis <svenheis@students.ibaw.ch>
2024-05-15 22:59:54 +0200

    anpassung von cors für render

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(cookie());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://moneybook-frontend.onrender.com",
   })
 );
 const salt = bcrypt.genSaltSync(10);
