deff4d220089fac387a1b1419dd1473e0101d80e
Sven Heis <svenheis@students.ibaw.ch>
2024-05-17 00:33:44 +0200

    cookieparser correkt

--- src/routes/eintragRoute.js
+++ src/routes/eintragRoute.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express.Router();
 const controller = require("../controller/eintragController");
 const jwt = require("jsonwebtoken");
+const cookie = require("cookie-parser");
 
 dotenv.config();
+app.use(cookie());
 
 const verifyToken = async (req, res, next) => {
   console.log(req.cookies.token);
