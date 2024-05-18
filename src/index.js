11abfca0faa744e1a590619768e1f2c76532a7e3
Sven Heis <svenheis@students.ibaw.ch>
2024-05-18 21:19:34 +0200

    x

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
-const cookie = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 // Lesen der geheimen Informationen
 dotenv.config();
@@ -14,21 +14,7 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
-app.use(cookie());
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://moneybook-frontend.onrender.com"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cookieParser());
 
 app.use(
   cors({
