e40c0652133187e52151f1378dbd2edd2bf96870
Sven Heis <svenheis@students.ibaw.ch>
2024-05-19 23:39:16 +0200

    bereit für Render.com

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://moneybook-frontend.onrender.com",
   credentials: true,
 };
 
