18627f7c0c2d45474d04174a98b9fbf0f993d3e0
Sven Heis <svenheis@students.ibaw.ch>
2024-05-29 23:26:18 +0200

    url anpassung

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,17 +3,7 @@ const router = express.Router();
 const controller = require("../controller/userController");
 const { check } = require("express-validator");
 
-router.post(
-  "/register",
-
-  [
-    check("userName").not().isEmpty(),
-    check("email").not().isEmpty(),
-    check("password").isLength({ min: 4 }),
-  ],
-
-  controller.registrieren
-);
+router.post("/register", controller.registrieren);
 router.post("/login", controller.anmelden);
 router.get("/user", controller.userAusgeben);
 router.get("/logout", controller.logout);
